ac52af9d1cbcd65fb0771e9d37f9e453f90febfa
Waqas <waqasn229226@gmail.com>
2022-12-11 00:57:49 +0500

    third

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Variables in java Script
 
 // var myName = "Syed Waqas Ahmed";
 // var _myAge =26;
@@ -14,3 +14,75 @@
 // //var $myName = "Waqas Ghuru"
 
 // console.log(_1my__Name)
+
+// DataTypes in java Script
+
+// var myName = "Waqas Naeem";
+// console.log(myName);
+
+// var myAge = 25;
+// console.log(myAge);
+
+// var iAmWaqass =false;
+// console.log(iAmWaqass);
+
+// //typeof Operator
+// console.log(typeof(iAmWaqass))
+
+//DataTypes Practice
+
+// console.log(10+"20");
+// // 9-"5"
+// console.log(9-"5"); //bug
+// "java"+"script"
+// console.log("java "+"script"); 
+// " "+" "
+// console.log(" "+" "); 
+// " "+0
+// console.log(" "+0); 
+// "waqas" - "Naeem"
+// console.log("waqas" - "Naeem"); Nan//
+// true + true
+// console.log(true + true);
+// true + false
+// console.log(true + false);
+// false + true
+// console.log(false + true);
+// false - true
+// console.log(false - true);
+
+
+//interview Question 1 
+//D/B null vs undefined...
+
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof (iAmUseless))
+// //2 JavaScript Bug
+
+// var iAmStandBy;
+// console.log(iAmStandBy);
+// console.log(typeof (iAmStandBy))
+
+//Interview Question 2 what is Nan?
+
+
+// var myPhoneNumber = 9876543210;
+// var myName ="Syed Waqas Naeem";
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// if(isNaN(myName)){
+//     console.log("plz enter valid phone no")
+// }
+
+// Nan Practice
+
+// NaN === NaN;
+// Number.NaN === NaN;
+// isNaN(NaN);
+// isNaN(Number.NaN);
+// Number.isNaN(NaN);
+
+// console.log(Number.isNaN(NaN));
