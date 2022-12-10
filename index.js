79eb62a2d78a940ca8faa1911058ac1896efdc00
Waqas <waqasn229226@gmail.com>
2022-12-10 23:48:43 +0500

    hasan

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+// var myName = "Syed Waqas Ahmed";
+// var _myAge =26;
+
+// console.log(_myAge);
+
+ 
+//Naming Practice
+
+//var _myName = "Syed";
+// //var 1myName ="Waqas";
+// var _1my__Name = "Naeem"
+// //var $myName = "Waqas Ghuru"
+
+// console.log(_1my__Name)
